f842a36a75b345be56a4b5bbffe700609ff8a0de
Daniel Purrington <trex1883@gmail.com>
2020-02-22 21:21:21 -0700

    fix port

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
-import express from 'express'
-// import bodyParser from 'body-parser'
-import mongoose from 'mongoose'
+const path = require('path');
 
-const app = express()
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
+const errorController = require('./controllers/error');
+const User = require('./models/user');
 
+const app = express();
 
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
 
-
+app.use(errorController.get404);
 
 
 mongoose
@@ -18,10 +29,13 @@ mongoose
     `mongodb+srv://daniel:Dragon35!@cluster0-taek1.mongodb.net/test?retryWrites=true&w=majority`,
   )
   .then(result => {
-    app.listen(port, () => {
-      console.log(
-        `Server is running on port ${port}`
-      )
-    })
+    app.listen(3000);
   })
+//   .then(result => {
+//     app.listen(port, () => {
+//       console.log(
+//         `Server is running on port ${port}`
+//       )
+//     })
+//   })
   .catch(err => console.log(err))
