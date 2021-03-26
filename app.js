01285eb08a7fb1801386dbcf74c751195be4dbe3
Daniel Purrington <trex1883@gmail.com>
2021-03-25 20:29:16 -0600

    Updates and add Security

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const path = require('path');
 
+require('dotenv').config();
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
@@ -31,8 +33,7 @@ if (port == null || port == ''){
 
 
 mongoose
-  // .connect('mongodb+srv://daniel:Abcd1234!@cluster0-taek1.mongodb.net/test?retryWrites=true&w=majority')
-  .connect('mongodb+srv://daniel:Dragon35!@cluster0-taek1.mongodb.net/test?retryWrites=true&w=majority')
+  .connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(result => {
     app.listen(port, () => {
       console.log(
